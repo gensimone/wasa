57473642bc6b6a5a668af883e958b57a4f8d2271
Simone Gentili <gensimone.git@gmail.com>
2026-05-07 14:43:55 +0200

    Update main.go fmt.

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -32,13 +32,13 @@ import (
 	"github.com/gensimone/WASA-project/service/api"
 	"github.com/gensimone/WASA-project/service/database"
 	"github.com/gensimone/WASA-project/service/globaltime"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
